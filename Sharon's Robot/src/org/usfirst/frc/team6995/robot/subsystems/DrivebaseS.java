bd255046bae608773be238fcce73333382409589
Sharon Rigg <riggfamily@cox.net>
2018-09-27 19:58:25 -0700

    Added motor settings
    Added setSafetyEnabled and setNeutralMode

--- Sharon's Robot/src/org/usfirst/frc/team6995/robot/subsystems/DrivebaseS.java
+++ Sharon's Robot/src/org/usfirst/frc/team6995/robot/subsystems/DrivebaseS.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team6995.robot.subsystems;
 import org.usfirst.frc.team6995.robot.RobotMap;
 import org.usfirst.frc.team6995.robot.commands.DriveArcadeC;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -25,6 +26,12 @@ public class DrivebaseS extends Subsystem {
 		driveLeft = new WPI_TalonSRX(RobotMap.DRIVEBASE_LEFT_TALON_CAN_ID);
 		driveRight = new WPI_TalonSRX(RobotMap.DRIVEBASE_RIGHT_TALON_CAN_ID);
 		differentialDrive = new DifferentialDrive(driveLeft, driveRight);
+		
+		driveLeft.setNeutralMode(NeutralMode.Brake);
+		driveRight.setNeutralMode(NeutralMode.Brake);
+		
+		driveLeft.setSafetyEnabled(false);
+		driveRight.setSafetyEnabled(false);
 	}
 
 
