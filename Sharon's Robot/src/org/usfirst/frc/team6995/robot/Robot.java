db00e610a15483692c9f28dc8a4e466c52cba923
Sharon Rigg <riggfamily@cox.net>
2018-09-27 20:26:17 -0700

    New Branch to test Preferences

--- Sharon's Robot/src/org/usfirst/frc/team6995/robot/Robot.java
+++ Sharon's Robot/src/org/usfirst/frc/team6995/robot/Robot.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team6995.robot.commands.DriveArcadeC;
 import org.usfirst.frc.team6995.robot.subsystems.DrivebaseS;
 
-/** Sharon's Robot Code
+/** Sharon's Robot Code - Testing Preferences
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
